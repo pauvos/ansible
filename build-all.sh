26eeda67d1f1fc1696b9fb02680079beba42b142
Tobias Trabelsi <lerentis@burntbunch.org>
2019-11-17 18:15:09 +0100

    using buildx to support multiarch. sadly not available for drone :/

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd ubuntu-18.04 && docker build -t lerentis/ansible:ubuntu-18.04 .
-cd ubuntu-16.04 && docker build -t lerentis/ansible:ubuntu-16.04 .
-cd ../alpine-3 && docker build -t lerentis/ansible:alpine-3 .
-cd ../ubuntu-14.04  && docker build -t lerentis/ansible:ubuntu-14.04 .
-cd ../fedora-24 && docker build -t lerentis/ansible:fedora-24 .
-cd ../debian-8 && docker build -t lerentis/ansible:debian-8 .
-cd ../debian-7 && docker build -t lerentis/ansible:debian-7 .
-cd ../centos-7 && docker build -t lerentis/ansible:centos-7 .
-cd ../arch && docker build -t lerentis/ansible:arch .
+cd ubuntu-18.04 && docker buildx build -t lerentis/ansible:ubuntu-18.04 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../ubuntu-16.04 && docker buildx build -t lerentis/ansible:ubuntu-16.04 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../alpine-3 && docker buildx build -t lerentis/ansible:alpine-3 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../ubuntu-14.04  && docker buildx build -t lerentis/ansible:ubuntu-14.04 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../fedora-24 && docker buildx build -t lerentis/ansible:fedora-24 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../debian-8 && docker buildx build -t lerentis/ansible:debian-8 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../debian-7 && docker buildx build -t lerentis/ansible:debian-7 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../centos-7 && docker buildx build -t lerentis/ansible:centos-7 --platform=linux/arm,linux/arm64,linux/amd64 . --push
+cd ../arch && docker buildx build -t lerentis/ansible:arch --platform=linux/arm,linux/arm64,linux/amd64 . --push
